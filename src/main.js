9ec889e1ec3fb888ce35aef84e2956db019481cf
Eli White <github@eli-white.com>
2016-01-23 10:49:43 -0800

    Removing comments

--- src/main.js
+++ src/main.js
@@ -1,24 +1,6 @@
-// Game.spawns.Spawn1.createCreep( [WORK, CARRY, MOVE], 'Worker1' );
-// Game.spawns.Spawn1.createCreep( [WORK, WORK, CARRY, MOVE], 'Builder1' );
-// module.exports.loop = function () {
-//   var creep = Game.creeps.Worker1;
-//   var sources = creep.room.find(FIND_SOURCES);
-//   if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-//     creep.moveTo(sources[0]);
-//   }
-// }
-
-
-// Memory.creeps.Worker1.role = 'harvester';
-// Memory.creeps.Worker2.role = 'harvester';
-// Memory.creeps.Builder1.role = 'builder';
-
-
 var harvester = require('harvester');
 var builder = require('builder');
 var guard = require('guard');
-
-// Game.spawns.Spawn1.createCreep( [TOUGH, ATTACK, MOVE, MOVE], 'Guard1', { role: 'guard' } );
 
 var harvesters = [];
 var guards = [];
